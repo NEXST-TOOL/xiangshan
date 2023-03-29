c5b164c86666d1a2a1f70c7061cb4fb12d7cc89c
imhcyx <git@iwannaknow.me>
2023-03-29 14:31:58 +0800

    config: add configs for FPGA prototyping

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -282,7 +282,7 @@ class WithNKBL3(n: Int, ways: Int = 8, inclusive: Boolean = true, banks: Int = 1
           address = 0x39000000,
           numCores = tiles.size
         )),
-        sramClkDivBy2 = true,
+        sramClkDivBy2 = false,
         sramDepthDiv = 4,
         tagECC = None,
         dataECC = None,
@@ -451,6 +451,34 @@ class NanHuGFPGAConfig(n: Int = 1) extends Config(
   new NanHuGConfig(n).alter((site, here, up) => {
     case DebugOptionsKey => up(DebugOptionsKey).copy(
       AlwaysBasicDiff = false
+    )
+  })
+)
+
+class NanHuGServeConfig(n: Int = 1) extends Config(
+  new NanHuGFPGAConfig(n).alter((site, here, up) => {
+    case SoCParamsKey => up(SoCParamsKey).copy(
+      L3CacheParamsOpt = Some(up(SoCParamsKey).L3CacheParamsOpt.get.copy(
+        sramClkDivBy2 = false,
+      ))
+    )
+  })
+)
+
+class MinimalFPGAConfig(n: Int = 1) extends Config(
+  new MinimalConfig(n).alter((site, here, up) => {
+    case DebugOptionsKey => up(DebugOptionsKey).copy(
+      AlwaysBasicDiff = false
+    )
+  })
+)
+
+class NanHuGServeConfig(n: Int = 1) extends Config(
+  new NanHuGFPGAConfig(n).alter((site, here, up) => {
+    case SoCParamsKey => up(SoCParamsKey).copy(
+      L3CacheParamsOpt = Some(up(SoCParamsKey).L3CacheParamsOpt.get.copy(
+        sramClkDivBy2 = false,
+      ))
     )
   })
 )
